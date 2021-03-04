31f45366fe8694ce48e5a864f5626c506d635732
repl.it user <replituser@example.com>
2021-03-04 10:10:37 +0000

    techcommit

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -2,5 +2,23 @@
 # define FT_PING_H
 
 # include "libft.h"
+# include <sys/socket.h>
+# include <netdb.h>
+# include <sys/time.h>
+# include <arpa/inet.h>
+# include <signal.h>
+
+# define ALLOWED_OPT "vh"
+
+typedef struct 			s_ping
+{
+	pid_t				pid;
+	uid_t				uif;
+	struct	timeval		*tv;
+	struct	timezone	*tz;
+	struct	addrinfo	*res;
+	struct	msghdr		*msg;
+	
+}						t_ping;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -5,8 +5,7 @@ int				main(int argc, char	**argv)
 	int			i;
 
 	i = 1;
-	printf("It is the very beginning %d:\n", argc);
 	while (i < argc)
 		printf("*-> %s\n", argv[i]);
-	return (0);
+	exit (EXIT_SUCCESS);
 }
